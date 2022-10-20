b7cd38faf7284c4c578b53ba6632cd5da1165925
neyssacadet <neyssacadet@gmail.com>
2022-10-20 06:22:56 -0500

    added basic operations and menu selection

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        operations operation = new operations();
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("What operation would you like to perform?");
+        System.out.println("1. Addition");
+        System.out.println("2. Substraction");
+        System.out.println("3. Multiplication");
+        System.out.println("4. Division");
+        System.out.println("5. EXIT");
+
+
+
+
+
+    }
+}
---
+++ src/operations.java
@@ -0,0 +1,20 @@
+public class operations {
+    //methods that perform basic math operations on 2 operands
+    public float addition (float operandone, float operandtwo){
+        float sum = operandone + operandtwo;
+        return sum;
+    }
+    public float substraction (float operandone, float operandtwo){
+        float substract = operandone + operandtwo;
+        return substract;
+    }
+    public float multiplication (float operandone, float operandtwo){
+        float multiply = operandone + operandtwo;
+        return multiply;
+    }
+    public float division (float operandone, float operandtwo){
+        float divide = operandone + operandtwo;
+        return divide;
+    }
+
+}
