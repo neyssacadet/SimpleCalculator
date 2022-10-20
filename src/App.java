7f6c374066431f86ec676c5b42a6560651d0b3ea
neyssacadet <neyssacadet@gmail.com>
2022-10-20 06:33:45 -0500

    ability to perform basic operations

--- src/App.java
+++ src/App.java
@@ -11,10 +11,31 @@ public class App {
         System.out.println("3. Multiplication");
         System.out.println("4. Division");
         System.out.println("5. EXIT");
+        int entry = myScanner.nextInt();
 
+        System.out.println("Your first operand?");
+        float operandone = myScanner.nextFloat();
+        System.out.println("Your second operand?");
+        float operandtwo = myScanner.nextFloat();
 
-
-
-
+        switch(entry){
+            case 1:
+                System.out.println("The sum is: " + operation.addition(operandone, operandtwo));
+                break;
+            case 2:
+                System.out.println("The difference is: " + operation.substraction(operandone, operandtwo));
+                break;
+            case 3:
+                System.out.println("The multiplication is equal to: " + operation.multiplication(operandone, operandtwo));
+                break;
+            case 4:
+                System.out.println("The division is equal to: " + operation.division(operandone, operandtwo));
+                break;
+            case 5:
+                System.out.println("CIAO!");
+                break;
+            default:
+                System.out.println("UNKNOWN COMMAND");
+        }
     }
 }
